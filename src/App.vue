c1ffd7d3115f923a5b4966f5ae4973d874202b36
Adentalis <daniel.n96@t-online.de>
2022-10-15 13:25:46 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>asdsad</h1>
+    <div class="content">
+      <div class="item">x</div>
+      <div class="item">x</div>
+      <div class="item">x</div>
+      <div class="item">x</div>
+      <div class="item">x</div>
+      <div class="item">x</div>
+      <div class="item">x</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+  border: 2px red solid;
+}
+
+.content {
+  border: 3px green solid;
+  display: grid;
+  grid-template-rows: 1fr 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+}
+
+.item {
+  min-height: 200px;
+  min-width: 200px;
+
+  border: 3px yellow solid;
+}
+</style>
